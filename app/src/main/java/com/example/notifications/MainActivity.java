53cf4d40135271612ca41ce925cf27859e7856f6
Siddhu <gunjisiddhu@gmail.com>
2022-04-05 11:17:45 +0530

    Media Notifications Added

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.Notification.MediaStyle;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.*;
 
 import android.app.NotificationManager;
@@ -18,8 +21,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText title,desc;
-    Button c1,c2;
+    EditText title, desc;
+    Button c1, c2, c3, c4, c5;
     private static int id = 0;
     NotificationManagerCompat manager;
 
@@ -33,21 +36,23 @@ public class MainActivity extends AppCompatActivity {
         desc = findViewById(R.id.desc);
         c1 = findViewById(R.id.channel1);
         c2 = findViewById(R.id.channel2);
-
+        c3 = findViewById(R.id.channel3);
+        c4 = findViewById(R.id.channel4);
+        c5 = findViewById(R.id.channel5);
 
 
         c1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Notification notification = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_1_ID)
+                Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_1_ID)
                         .setSmallIcon(R.drawable.ic_airport)
                         .setContentTitle(title.getText().toString())
                         .setContentText(desc.getText().toString())
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
                         .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                         .build();
-                manager.notify(id++,notification);
+                manager.notify(id++, notification);
             }
         });
 
@@ -55,14 +60,62 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Notification notification = new NotificationCompat.Builder(getApplicationContext(),CHANNEL_2_ID)
+                Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_2_ID)
                         .setSmallIcon(R.drawable.ic_tractor)
                         .setContentTitle(title.getText().toString())
                         .setContentText(desc.getText().toString())
                         .setPriority(NotificationCompat.PRIORITY_HIGH)
                         .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                         .build();
-                manager.notify(id++,notification);
+                manager.notify(id++, notification);
+            }
+        });
+
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Bitmap large = BitmapFactory.decodeResource(getResources(), R.drawable.large);
+                Bitmap small = BitmapFactory.decodeResource(getResources(),R.drawable.small);
+                Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_1_ID)
+                        .setSmallIcon(R.drawable.ic_airport)
+                        .setContentTitle(title.getText().toString())
+                        .setContentText(desc.getText().toString())
+                        .setLargeIcon(small)
+                        .setStyle(new NotificationCompat.BigPictureStyle()
+                                .bigPicture(large)
+                                .bigLargeIcon(null))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .build();
+                manager.notify(id++, notification);
+            }
+        });
+
+        c4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Bitmap large = BitmapFactory.decodeResource(getResources(), R.drawable.large);
+                Bitmap small = BitmapFactory.decodeResource(getResources(),R.drawable.small);
+
+                Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_2_ID)
+                        .setSmallIcon(R.drawable.ic_tractor)
+                        .setContentTitle(title.getText().toString())
+                        .setLargeIcon(small)
+                        .setContentText(desc.getText().toString())
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                        .addAction(R.drawable.ic_dislike,"dislike",null)
+                        .addAction(R.drawable.ic_previous,"previous",null)
+                        .addAction(R.drawable.ic_pause,"pause",null)
+                        .addAction(R.drawable.ic_next,"next",null)
+                        .addAction(R.drawable.ic_like,"like",null)
+                        .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                            .setShowActionsInCompactView(1,2,3))
+                        .setSubText("Playing now")
+                        .build();
+                manager.notify(id++, notification);
             }
         });
 
--- app/src/main/java/com/example/notifications/NotificationChannels.java
+++ app/src/main/java/com/example/notifications/NotificationChannels.java
@@ -6,8 +6,11 @@ import android.app.NotificationManager;
 import android.os.Build;
 
 public class NotificationChannels extends Application {
-    public static final String CHANNEL_1_ID = "1000";
-    public static final String CHANNEL_2_ID = "1001";
+    public static final String CHANNEL_1_ID = "1001";
+    public static final String CHANNEL_2_ID = "1002";
+    public static final String CHANNEL_3_ID = "1003";
+    public static final String CHANNEL_4_ID = "1004";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -15,13 +18,20 @@ public class NotificationChannels extends Application {
         createChannels();
     }
     private void createChannels(){
+
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+
+
+
             NotificationChannel notificationChannel1 = new NotificationChannel(
                     CHANNEL_1_ID,
                     "channel 1",
                     NotificationManager.IMPORTANCE_DEFAULT
             );
             notificationChannel1.setDescription("testing channel 1");
+
+
             NotificationChannel notificationChannel2 = new NotificationChannel(
                     CHANNEL_2_ID,
                     "channel 2",
@@ -29,9 +39,31 @@ public class NotificationChannels extends Application {
             );
             notificationChannel2.setDescription("Testing channel 2");
 
+            NotificationChannel notificationChannel3 = new NotificationChannel(
+                    CHANNEL_3_ID,
+                    "channel 3",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            notificationChannel3.setDescription("This is channel 3");
+
+            NotificationChannel notificationChannel4 = new NotificationChannel(
+                    CHANNEL_4_ID,
+                    "channel 4",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            notificationChannel3.setDescription("This is channel 4");
+
+
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(notificationChannel1);
             manager.createNotificationChannel(notificationChannel2);
+            manager.createNotificationChannel(notificationChannel3);
+            manager.createNotificationChannel(notificationChannel4);
+
+
+
+
+
         }
     }
 }
